cb1bf573b0d40d91b05e52a67f453b9c85205296
Hoàng Đức Việt <viethd@sixthgearstudios.com>
2020-04-27 21:11:58 +0700

    Auto clicker
    Still buggy but you'll even get more time

--- start.py
+++ start.py
@@ -1,28 +1,36 @@
+import time
 import numpy as np
 import cv2
 from mss import mss
-from PIL import Image
+from PIL import Image, ImageEnhance
+from pynput.mouse import Button, Controller, Listener
 
 def find_different(arr):
     for num,i in enumerate(arr):
         if (arr.count(i)==1):
             return num
-    else:
-        return 0
+    return -1
         
-mon = {'top': 179, 'left': 706, 'width': 1214-706, 'height': 1080-179}
-
+mon = {'top': 400, 'left': 1200, 'width': 400, 'height': 400}
+mouse = Controller()
+click_delay = .4 #secs
 sct = mss()
+start = time.time()
+
+def on_click():
+    time.sleep(click_delay)
+Listener(on_click=on_click)
 
 while 1:
     sct.get_pixels(mon)
-    img = np.array(Image.frombytes('RGB', (sct.width, sct.height), sct.image))
-    
-    
-    
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+    converter = ImageEnhance.Color(img)
+    img2 = np.array(converter.enhance(2))
 
+    img = np.array(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT,1.2,10)
+    output = img2
     
     if circles is not None:
         # convert the (x, y) coordinates and radius of the circles to integers
@@ -35,21 +43,28 @@ while 1:
             # corresponding to the center of the circle
         #print(circles)
         nn = find_different(this)
+        
         for num,(x, y, r) in enumerate(circles):
-            if num==nn:
-                cv2.circle(img, (x, y), r, (0, 255, 0), 4)
-                cv2.rectangle(img, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            if num == nn:
+                cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+                # Move mouse to circle with delay
+                # if time.time() > start + click_delay:
+                #     start = time.time()
+                # Mac user with Retina x2 need to divide by 2
+                mouse.position = (mon['left'] + x, mon['top'] + y)
+                mouse.position = (mon['left'] + x, mon['top'] + y)
+                mouse.click(Button.left)
+                time.sleep(click_delay)
+                break
             
         
-            
-    cv2.imshow('test', np.array(img))
+    # cv2.imshow('vit', np.array(img))
+    cv2.imshow('vit', np.array(output))
+    cv2.moveWindow('vit', mon['left'] - mon['width'] - 80, mon['top']) 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
 
-
-
-
-
-
-
+    
